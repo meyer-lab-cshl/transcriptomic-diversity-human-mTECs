1e01eda1916fde57187167fdc91ebf861d0356fa
Hannah Meyer <hannah.v.meyer@gmail.com>
2019-05-14 09:08:39 -0400

    Move files

--- meme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-file=$file
-outdir=$outdir
-mode=$mode
-no_motifs=$no_motifs
-
-/home/stroemic/software/miniconda2/envs/rnaseq/bin/meme $file -oc $outdir -dna -mod $mode -nmotifs $no_motifs 
--- split_read_pairs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xdir=$filedir
-name=$name
-
-echo '1' 
-/home/stroemic/software/miniconda2/envs/rnaseq/bin/samtools view -h -f 0x40  $xdir/${name}_Aligned.out.unique.sorted.mbc.sam > $xdir/${name}_Aligned.out.unique.sorted.mbc._1.sam
-echo '2'
-/home/stroemic/software/miniconda2/envs/rnaseq/bin/samtools view -h -f 0x80  $xdir/${name}_Aligned.out.unique.sorted.mbc.sam > $xdir/${name}_Aligned.out.unique.sorted.mbc._2.sam
