9c4aa98d3e6027f4e99736314bf91e97968d7ec5
Hannah Meyer <hannah.v.meyer@gmail.com>
2019-10-04 11:15:53 -0400

    Fix filepaths

--- human/make_dag_processing_alignment.sh
+++ human/make_dag_processing_alignment.sh
@@ -1,3 +1,3 @@
 snakemake -s processing_alignment_human.smk \
-    --dag /mnt/grid/meyer/hpc/home/data/common/tss/2_alignments/pt221-lo_5prime_nz_readdepth.combined.ucsc.bedgraph \
-    /mnt/grid/meyer/hpc/home/data/common/tss/3_tss_data/raw_positions/single_samples/pt221-lo_fwd.positions.csv |  dot -Tpdf > processing_alignment_human_dag.pdf
+    --dag ~/data/tss/human/2_alignments/pt221-lo_5prime_nz_readdepth.combined.ucsc.bedgraph \
+    ~/data/tss/combined/3_tss_data/raw_positions/all_mTECs_fwd.positions.csv |  dot -Tpdf > processing_alignment_human_dag.pdf
--- mouse/make_dag_processing_alignment.sh
+++ mouse/make_dag_processing_alignment.sh
@@ -1,3 +1,3 @@
 snakemake -s processing_alignment_mouse.smk \
     --dag ~/data/tss/mouse/2_alignments/mESC1_1_5prime_nz_readdepth.combined.bedgraph \
-    ~/data/tss/combined/3_tss_data/raw_positions/single_samples/mESC1_1.positions.csv |  dot -Tpdf > processing_alignment_mouse_dag.pdf
+    ~/data/tss/combined/3_tss_data/raw_positions/all_mESCs.positions.csv |  dot -Tpdf > processing_alignment_mouse_dag.pdf
