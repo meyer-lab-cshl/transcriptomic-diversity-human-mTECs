7b506524ddd6578be2e109549876d9d740e2aab0
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-02-07 11:50:40 -0500

    Update alignment with common tools

--- human/make_dag_alignment.sh
+++ human/make_dag_alignment.sh
@@ -1,5 +1,5 @@
-snakemake -s alignment_human.smk \
-    --dag ~/data/tss/human/1_raw_data/5Pseq_processed/fastq/pt221-lo_1.fastq \
-    ~/data/tss/human/2_alignments/pt221-lo_Aligned.out.sam \
-    ~/data/tss/human/2_alignments/STAR_summary.pdf \
-    ~/data/common/public/annotations/human/genome/STARINDEX/Genome|  dot -Tpdf > alignment_human_dag.pdf
+snakemake -s alignment.smk \
+    --rulegraph ~/data/tss/human/5Pseq/fastq/pt221-lo_1.fastq \
+    ~/data/tss/human/5Pseq/deduplicated/pt221-lo_Aligned.sortedByCoord.dedup.bam \
+    ~/data/tss/human/5Pseq/multiqc/multiqc_report.html \
+    ~/data/tss/human/5Pseq/alignment_qc/multiqc_report.html |  dot -Tpdf > alignment_dag.pdf
