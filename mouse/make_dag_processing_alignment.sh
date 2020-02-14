de3b83ee136d01c49ecb74b0076f4f6d0d389ef9
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-02-14 15:17:49 -0500

    Change dag to rulegraphs

--- mouse/make_dag_processing_alignment.sh
+++ mouse/make_dag_processing_alignment.sh
@@ -1,3 +1,3 @@
 snakemake -s processing_alignment.smk \
-    --dag ~/data/tss/mouse/5Pseq/bedgraphs/mESC1_1_5prime_nz_readdepth.combined.bedgraph \
+    --rulegraph ~/data/tss/mouse/5Pseq/bedgraphs/mESC1_1_5prime_nz_readdepth.combined.bedgraph \
     ~/data/tss/mouse/5Pseq/tss/combined/all_mESCs.positions.csv |  dot -Tpng > dag/processing_alignment_dag.png
