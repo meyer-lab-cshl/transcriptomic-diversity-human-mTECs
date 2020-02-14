7903b8ed61f8dc15d74b2f3f59cc97dc722f30ef
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-02-14 15:20:27 -0500

    Update path for combined files

--- human/make_dag_processing_alignment.sh
+++ human/make_dag_processing_alignment.sh
@@ -1,3 +1,3 @@
 snakemake -s processing_alignment.smk \
     --rulegraph  ~/data/tss/human/5Pseq/bedgraphs/pt221-lo_5prime_nz_readdepth.combined.ucsc.bedgraph \
-    ~/data/tss/human/5Pseq/tss/raw_positions/all_samples_fwd.positions.csv |  dot -Tpng > dag/processing_alignment_dag.png
+    ~/data/tss/human/5Pseq/tss/combined/all_samples_fwd.positions.csv |  dot -Tpng > dag/processing_alignment_dag.png
