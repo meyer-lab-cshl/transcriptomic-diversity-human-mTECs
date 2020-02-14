6d40efe794c4db63e57f0040389da4b6902e7614
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-02-14 15:17:49 -0500

    Change dag to rulegraphs

--- mouse/make_dag_alignment.sh
+++ mouse/make_dag_alignment.sh
@@ -1,4 +1,4 @@
 snakemake -s alignment.smk \
-    --dag ~/data/tss/mouse/5Pseq/alignments/mESC1_1_Aligned.sortedByCoord.out.bam \
+    --rulegraph ~/data/tss/mouse/5Pseq/alignments/mESC1_1_Aligned.sortedByCoord.out.bam \
     ~/data/tss/mouse/5Pseq/multiqc/multiqc_report.html \
     ~/data/tss/mouse/5Pseq/alignment_qc/multiqc_report.html | dot -Tpng > dag/alignment_dag.png
