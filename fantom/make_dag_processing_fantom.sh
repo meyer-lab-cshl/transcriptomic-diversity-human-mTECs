176f92d30e79d9b64025c1eeecd349e5ef4c4397
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-02-21 11:55:23 -0500

    update rulegraph

--- fantom/make_dag_processing_fantom.sh
+++ fantom/make_dag_processing_fantom.sh
@@ -1,3 +1,3 @@
 snakemake -s processing_fantom.smk \
-    --dag ~/data/tss/combined/3_tss_data/raw_positions/all_tissues.hg19.ctss.positions.csv \
-         ~/data/tss/combined/3_tss_data/raw_positions/all_mESC_46C.mm9.ctss.positions.csv |  dot -Tpdf > processing_fantom_dag.pdf
+    --rulegraph ~/data/tss/mouse/fantom/tss/combined/all_mESC_46C.positions.csv \
+    ~/data/tss/human/fantom/tss/combined/all_tissues.positions.csv |  dot -Tpng > dag/processing_fantom_dag.png
