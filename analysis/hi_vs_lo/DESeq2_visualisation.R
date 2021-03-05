9f90d17e3ec5fedcca5adcd2d30cbc7fe320121a
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-05 16:18:02 -0500

    Updated DESeq2 script to associate gene IDs with gene names

--- analysis/hi_vs_lo/DESeq2_visualisation.R
+++ analysis/hi_vs_lo/DESeq2_visualisation.R
@@ -7,4 +7,17 @@ library(ggplot2)
 ## Differential expression results: res
 ## Transformed counts: vs_dds
 
+## PCA
+
 plotPCA(vs_dds, intgroup = 'group') + theme_bw()
+
+## Count plots for individual genes
+
+par(mfrow=c(2,2))
+plotCounts(dds, gene = 'ENSG00000160224.17', intgroup = 'group') ## AIRE
+plotCounts(dds, gene = 'ENSG00000121594.12', intgroup = 'group') ## CD80
+plotCounts(dds, gene = 'ENSG00000153266.13', intgroup = 'group') ## FEZF2
+plotCounts(dds, gene = '', intgroup = 'group') ## FEZF2
+
+## Volcano
+
