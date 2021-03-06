70731a283e01aaa5b05ad7b3e5950a8eaf826c3a
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-06 12:12:59 -0500

    Prettified TE volcano plot

--- analysis/hi_vs_lo/DESeq2_visualisation_TEs.R
+++ analysis/hi_vs_lo/DESeq2_visualisation_TEs.R
@@ -29,28 +29,34 @@ df2 = filter(df, Class == 'protein_coding')
 library(EnhancedVolcano)
 
 EnhancedVolcano(df,
-                lab = df$GeneSymbol,
+                lab = rownames(df),
                 x = 'log2FoldChange',
-                selectLab = c('AIRE', 'CD80', 'FEZF2'),
                 y = 'padj',
                 title = 'mTEC-lo vs mTEC-hi gene expression',
                 pCutoff = 0.05)
 
-ggplot(data = df, aes(x = log2FoldChange, y = -log10(padj), colour = significant)) +
-  geom_point(alpha = 0.6) +
-  geom_vline(xintercept = c(1, -1), linetype = 'dashed') +
-  geom_hline(yintercept = -log10(0.05), linetype = 'dashed') +
-  xlab(expression('Log'[2])) +
-  ylab(expression('-Log'[10])) +
-  ggtitle('mTEC-lo vs mTEC-hi', 'TE expression') +
-  scale_colour_manual(values = c('#9B9A99', "red")) +
-  guides(colour = FALSE) +
-  theme_pubr() +
-  theme(panel.grid.major = element_line(colour = "gray")) +
-  geom_text_repel(
-    data = subset(df, significant == TRUE),
-    aes(label = rownames(subset(df, significant == TRUE))),
-    size = 3)
+volcano_plot = ggplot(data = df, aes(x = log2FoldChange, y = -log10(padj), colour = significant)) +
+                geom_point(alpha = 0.6) +
+                geom_vline(xintercept = c(1, -1), linetype = 'dashed') +
+                geom_hline(yintercept = -log10(0.05), linetype = 'dashed') +
+                xlab(expression('Log'[2])) +
+                ylab(expression('-Log'[10])) +
+                xlim(-3, 3) +
+                ggtitle('mTEC-lo vs mTEC-hi', 'TE expression') +
+                scale_colour_manual(values = c('#9B9A99', "red")) +
+                guides(colour = FALSE) +
+                geom_text_repel(
+                data = subset(df, significant == TRUE),
+                aes(label = rownames(subset(df, significant == TRUE))),
+                size = 4)
+
+volcano_plot + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 20),
+                                  plot.subtitle = element_text(size = 14),
+                                  axis.text.x = element_text(size = 14),
+                                  axis.text.y = element_text(size = 14),
+                                  axis.title = element_text(size = 14),
+                                  axis.line = element_line(size = 0.8),
+                                  panel.border = element_blank())
 
 ggsave("/Users/mpeacey/TE_thymus/analysis/hi_vs_lo/Plots/lo_vs_hi_TEs_volcano_plot.png", 
        width = 20, height = 15, units = "cm")
