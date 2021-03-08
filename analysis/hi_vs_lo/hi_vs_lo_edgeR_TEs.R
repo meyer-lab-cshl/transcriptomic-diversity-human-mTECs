32e2d6f3d199363aee74467be12bcf85c7355455
Matthew Peacey <mpeacey@Matty-P.local>
2021-03-08 14:54:11 -0500

    DESeq2 visualisations

--- analysis/hi_vs_lo/hi_vs_lo_edgeR_TEs.R
+++ analysis/hi_vs_lo/hi_vs_lo_edgeR_TEs.R
@@ -14,16 +14,26 @@ TE_data = data[grepl("^(?!ENSG).*$",rownames(data), perl = TRUE),]
 
 ## Create DGEList with filtering
 
-group <- c(1, 1, 1, 2, 2, 2)
+group = c('hi', 'hi', 'hi', 'lo', 'lo', 'lo')
+patient = c(214, 221, 226, 214, 221, 226)
 y = DGEList(counts = TE_data, group = group)
+
+y$samples$patient = patient
 
 keep = filterByExpr(y)
 y = y[keep, , keep.lib.sizes=FALSE]
 
 y = calcNormFactors(y)
 
-design = model.matrix(~group)
+design = model.matrix(~patient +group)
 y = estimateDisp(y,design)
 
-et <- exactTest(y)
-topTags(et)
+fit <- glmQLFit(y, design)
+tr <- glmTreat(fit, lfc=1)
+topTags(tr, n = 20, p.value = 0.1)
+
+#################################################################
+#################################################################
+# Data visualization
+#################################################################
+#################################################################
