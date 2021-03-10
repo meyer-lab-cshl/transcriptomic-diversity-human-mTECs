f965d825a5e9844a4cb9ce13441f92960261fe4b
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-10 16:07:12 -0500

    Generalised TE_local script

--- scripts/TE_local.sh
+++ scripts/TE_local.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe threads 1
-#$ -l m_mem_free=25G
+#$ -l m_mem_free=30G
 #$ -N TE_local
 #$ -o TE_local_output.txt
 #$ -e TE_local_output.txt
 
 cd $TE_HOME/data
 
-TElocal \
--b pt214_hi_fastp_1.fastq_Aligned.out.bam \
---GTF $TE_HOME/index/human.GRCh38.gtf \
---TE $TE_HOME/index/hg38_rmsk_TE.gtf.locInd \
---project TE_local_test
+for FILE in *.fastq_Aligned.out.bam; do
+  TElocal \
+  -b ${FILE} \
+  --GTF $TE_HOME/index/human.GRCh38.gtf \
+  --TE $TE_HOME/index/hg38_rmsk_TE.gtf.locInd \
+  --project TE_local_${FILE}
+
+done 
+
+
+
