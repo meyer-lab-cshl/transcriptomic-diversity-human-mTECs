6e6e2187970587bf16a3ead2161ed63fee035426
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-17 11:25:31 -0400

    New script for genomic position analysis with GenomicRanges and cooccur

---
+++ analysis/hi_vs_lo/hivslo_genomic_positon_analysis.R
@@ -0,0 +1,53 @@
+library(GenomicRanges)
+library(cooccur)
+library(dplyr)
+library(tidyr)
+
+#################################################################
+# 
+#################################################################
+
+make_GRanges = function(mode, results_df){
+  
+  if (mode == 'TE'){
+    
+    annotation = read.table(file = "/Users/mpeacey/TE_thymus/analysis/annotation_tables/hg38_rmsk_TE.gtf.locInd.locations.txt", header = 1)
+    annotation = separate(annotation, chromosome.start.stop, into = c('chr', 'start.stop'), sep = ':')
+    annotation = separate(annotation, start.stop, into = c('start', 'end'), sep = '-')
+    annotation = rename(annotation, locus = TE)
+    
+    df = merge(results_df, annotation, by = 'locus')
+    
+  }
+  
+  if (mode == 'gene'){
+    
+    annotation = read.table(file = '/Users/mpeacey/TE_thymus/analysis/annotation_tables/gencode.v38_gene_annotation_table.txt', header = 1)
+    annotation = select(annotation, c('Geneid', 'Chromosome', 'Start', 'End', 'Strand'))
+    annotation = rename(annotation, chr = Chromosome, start = Start, end = End, strand = Strand)
+    
+    df = merge(results_df, annotation, by = 'Geneid')
+    
+  }
+  
+  output = makeGRangesFromDataFrame(df, keep.extra.columns = T)
+  
+  return(output)
+  
+}
+
+##
+
+GRanges_gene = make_GRanges(mode = 'gene',
+                            results_df = results_df_local_gene)
+
+GRanges_gene_sigdiff = make_GRanges(mode = 'gene',
+                                    results_df = results_df_local_gene_sigdiff)
+
+
+GRanges_TE_sigdiff = make_GRanges(mode = 'TE',
+                                  results_df = results_df_local_TE_sigdiff)
+
+subsetByOverlaps(GRanges_TE_sigdiff, GRanges_gene_sigdiff)
+
+## Testing cooccur 
