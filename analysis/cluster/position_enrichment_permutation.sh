12a9e66f8a0411f01a45835492f0dffa5a2b958a
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-18 12:41:25 -0400

    Updated position_enrichment_permutation.sh to include module load

--- analysis/cluster/position_enrichment_permutation.sh
+++ analysis/cluster/position_enrichment_permutation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#$ -~TE_thymus/analysis/cluster/
-#$ -pe threads 16
-#$ -l m_mem_free=1G
+#$ -cwd
+#$ -pe threads 4
+#$ -l m_mem_free=10G
 #$ -N position_enrichment_permutation
 #$ -o position_enrichment_permutation_output.txt
 #$ -e position_enrichment_permutation_output.txt
 
+module load EBModules
+module load R/4.0.4-fosscuda-2020a
 Rscript position_enrichment_permutation.R
