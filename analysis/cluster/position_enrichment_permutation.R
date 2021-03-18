5e92dbb3a53739b3d56358782c28aa196dc41e81
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-18 14:57:33 -0400

    position_permutation.R new library path

--- analysis/cluster/position_enrichment_permutation.R
+++ analysis/cluster/position_enrichment_permutation.R
@@ -1,3 +1,5 @@
+.libPaths = '/grid/meyer/home/mpeacey/R/x86_64-pc-linux-gnu-library/4.0/'
+
 library(GenomicRanges)
 library(regioneR)
 
@@ -8,49 +10,80 @@ TE_groups = readRDS("~/TE_thymus/analysis/cluster/TE_groups.rds")
 
 ##########################
 
-output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
-rownames(output) = c('gene_up', 'gene_unchanged', 'gene_down')
-colnames(output) = c('TE_up', 'TE_unchanged', 'TE_down')
-
-row_number = 1
-number_of_tests = ncol(output) + nrow(output)
-
-for (gene_group in gene_groups){
+run_perm_test = function(gene_groups, TE_groups, mode = 'distance'){
   
-  column_number = 1
+  output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
+  rownames(output) = c('gene_up', 'gene_down')
+  colnames(output) = c('TE_up', 'TE_down')
   
-  for (TE_group in TE_groups){
+  row_number = 1
+  number_of_tests = ncol(output) + nrow(output)
+  
+  for (gene_group in gene_groups){
     
-    pt = permTest(A = TE_group, 
-                  B = gene_group, 
-                  ntimes = 1000,
-                  randomize.function = resampleRegions,
-                  universe = GRanges_TE,
-                  evaluate.function = meanDistance,
-                  alternative = 'less')
+    column_number = 1
     
-    p_value = pt$meanDistance[[1]]
-    Z_score = pt$meanDistance[[6]]
-    
-    if (p_value < (0.05/number_of_tests)){
+    for (TE_group in TE_groups){
       
-      output[row_number, column_number] = Z_score
+      print(glue('Starting column {column_number}, row {row_number}'))
+      
+      if (mode == 'distance'){
+        
+        pt = permTest(A = TE_group, 
+                      B = gene_group, 
+                      ntimes = 100,
+                      randomize.function = resampleRegions,
+                      universe = GRanges_TE,
+                      evaluate.function = meanDistance,
+                      alternative = 'less')
+        
+        p_value = pt$meanDistance[[1]]
+        Z_score = pt$meanDistance[[6]]
+        
+      }
+      
+      if (mode == 'overlap'){
+        
+        pt = permTest(A = TE_group, 
+                      B = gene_group, 
+                      ntimes = 100,
+                      randomize.function = resampleRegions,
+                      universe = GRanges_TE,
+                      evaluate.function = numOverlaps,
+                      alternative = 'greater')
+        
+        p_value = pt$numOverlaps[[1]]
+        Z_score = -pt$numOverlaps[[6]]
+        
+      }
+      
+      if (p_value < (0.05/number_of_tests)){
+        
+        output[row_number, column_number] = Z_score
+        
+      }
+      
+      else{
+        
+        output[row_number, column_number] = NA
+        
+      }
+      
+      column_number = column_number + 1
       
     }
     
-    else{
-      
-      output[row_number, column_number] = NA
-      
-    }
-    
-    column_number = column_number + 1
+    row_number = row_number + 1
     
   }
   
-  row_number = row_number + 1
+  return(output)
   
 }
+
+## Run
+
+output = run_perm_test(gene_groups, TE_groups, mode = 'overlap')
 
 # Export output
 
--- analysis/cluster/position_enrichment_permutation.sh
+++ analysis/cluster/position_enrichment_permutation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#$ -~TE_thymus/analysis/cluster/
+#$ -cwd
 #$ -pe threads 16
 #$ -l m_mem_free=1G
 #$ -N position_enrichment_permutation
 #$ -o position_enrichment_permutation_output.txt
 #$ -e position_enrichment_permutation_output.txt
 
+module load EBModules
+module load R/4.0.4-fosscuda-2020a
 Rscript position_enrichment_permutation.R
