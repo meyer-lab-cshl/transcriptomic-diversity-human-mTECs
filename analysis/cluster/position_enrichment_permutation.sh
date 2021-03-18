94c1be56bace0d8ef8d9229279035d0ccec3719a
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-18 15:16:46 -0400

    Undo git graffiti in my scripts

--- analysis/cluster/position_enrichment_permutation.sh
+++ analysis/cluster/position_enrichment_permutation.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 #$ -cwd
-<<<<<<< HEAD
-#$ -pe threads 16
-#$ -l m_mem_free=1G
-=======
 #$ -pe threads 4
 #$ -l m_mem_free=10G
->>>>>>> a542125d7fd15453297fca8c029d1913a02438c3
 #$ -N position_enrichment_permutation
 #$ -o position_enrichment_permutation_output.txt
 #$ -e position_enrichment_permutation_output.txt
