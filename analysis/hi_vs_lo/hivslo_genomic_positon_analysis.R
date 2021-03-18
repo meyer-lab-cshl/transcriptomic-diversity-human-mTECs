0328007b247e12629bbbe2cda192ce0d7b4a5df7
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-18 14:57:33 -0400

    position_permutation.R new library path

--- analysis/hi_vs_lo/hivslo_genomic_positon_analysis.R
+++ analysis/hi_vs_lo/hivslo_genomic_positon_analysis.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(ggplot2)
 library(pheatmap)
 library(RColorBrewer)
+library(glue)
 
 #################################################################
 # 
@@ -39,29 +40,83 @@ make_GRanges = function(mode, results_df){
   
 }
 
-## GRanges
-
-
-GRanges_gene = make_GRanges(mode = 'gene',
-                            results_df = results_df_local_gene)
-
-GRanges_TE = make_GRanges(mode = 'TE',
-                          results_df = results_df_local_TE)
-
-
-
-GRanges_gene_sigdiff = make_GRanges(mode = 'gene',
-                                    results_df = results_df_local_gene_sigdiff)
-
-
-GRanges_TE_sigdiff = make_GRanges(mode = 'TE',
-                                  results_df = results_df_local_TE_sigdiff)
-
-subsetByOverlaps(GRanges_TE_sigdiff, GRanges_gene_sigdiff)
+run_perm_test = function(gene_groups, TE_groups, mode = 'distance'){
+  
+  output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
+  rownames(output) = c('gene_up', 'gene_down')
+  colnames(output) = c('TE_up', 'TE_down')
+  
+  row_number = 1
+  number_of_tests = ncol(output) + nrow(output)
+  
+  for (gene_group in gene_groups){
+    
+    column_number = 1
+    
+    for (TE_group in TE_groups){
+      
+      print(glue('Starting column {column_number}, row {row_number}'))
+      
+      if (mode == 'distance'){
+        
+        pt = permTest(A = TE_group, 
+                      B = gene_group, 
+                      ntimes = 100,
+                      randomize.function = resampleRegions,
+                      universe = GRanges_TE,
+                      evaluate.function = meanDistance,
+                      alternative = 'less')
+        
+        p_value = pt$meanDistance[[1]]
+        Z_score = pt$meanDistance[[6]]
+        
+      }
+      
+      if (mode == 'overlap'){
+        
+        pt = permTest(A = TE_group, 
+                      B = gene_group, 
+                      ntimes = 100,
+                      randomize.function = resampleRegions,
+                      universe = GRanges_TE,
+                      evaluate.function = numOverlaps,
+                      alternative = 'greater')
+        
+        p_value = pt$numOverlaps[[1]]
+        Z_score = -pt$numOverlaps[[6]]
+        
+      }
+      
+      if (p_value < (0.05/number_of_tests)){
+        
+        output[row_number, column_number] = Z_score
+        
+      }
+      
+      else{
+        
+        output[row_number, column_number] = NA
+        
+      }
+      
+      column_number = column_number + 1
+      
+    }
+    
+    row_number = row_number + 1
+    
+  }
+  
+  return(output)
+  
+}
 
 #################################################################
 # GRanges
 ################################################################### 
+
+GRanges_TE = make_GRanges(mode = 'TE',
+                          results_df = results_df_local_TE)
 
 results_df_local_gene_up = filter(results_df_local_gene, (significant == T) & (log2FoldChange > 0))
 results_df_local_gene_unchanged = filter(results_df_local_gene, significant == F)
@@ -89,38 +144,15 @@ GRanges_TE_down = make_GRanges(mode = 'TE',
 # regioneR
 ################################################################### 
 
-random_RS = resampleRegions(GRanges_TE_sigdiff, universe=GRanges_TE)
-
-## Do my differentially expressed TEs overlap with differentially expressed genes more frequently than expected by chance?
-
-pt = permTest(A = GRanges_TE_sigdiff, 
-              B = GRanges_gene_sigdiff, 
-              ntimes = 100,
-              randomize.function = resampleRegions,
-              universe = GRanges_TE,
-              evaluate.function = numOverlaps)
-
-## Are differentially expressed TEs closer to differentially expressed genes than expected by chance?
-
-pt = permTest(A = GRanges_TE_sigdiff, 
-              B = GRanges_gene_sigdiff, 
-              ntimes = 100,
-              randomize.function = resampleRegions,
-              universe = GRanges_TE,
-              evaluate.function = meanDistance)
-
-pt_df = data.frame(permutations = pt$meanDistance[[5]])
-              
-ggplot() +
-  geom_histogram(data = pt_df, aes(x = permutations)) 
-
-## Heatmap
-
-gene_groups = list(GRanges_gene_up, GRanges_gene_unchanged, GRanges_gene_down)
-TE_groups = list(GRanges_TE_up, GRanges_TE_unchanged, GRanges_TE_down)
+gene_groups = list(GRanges_gene_up, GRanges_gene_down)
+TE_groups = list(GRanges_TE_up, GRanges_TE_down)
 
 saveRDS(gene_groups, "~/TE_thymus/analysis/cluster/gene_groups.rds")
 saveRDS(TE_groups, "~/TE_thymus/analysis/cluster/TE_groups.rds")
+
+output = run_perm_test(gene_groups, TE_groups, mode = 'overlap')
+
+## Heatmap
 
 my_heatmap = pheatmap(mat = output, 
                       cluster_rows=FALSE,
@@ -139,51 +171,7 @@ save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
   dev.off()
 }
 
-save_pheatmap_png(my_heatmap, "/Users/mpeacey/TE_thymus/analysis/Plots/TE_local/hi_vs_lo_genomic_poisitons_heatmap.png")
+save_pheatmap_png(my_heatmap, "/Users/mpeacey/TE_thymus/analysis/Plots/TE_local/hi_vs_lo_genomic_poisitons_heatmap_overlap.png")
 
 
 #####
-
-output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
-rownames(output) = c('gene_up', 'gene_down')
-colnames(output) = c('TE_up', 'TE_down')
-
-row_number = 1
-number_of_tests = ncol(output) + nrow(output)
-
-for (gene_group in gene_groups){
-  
-  column_number = 1
-  
-  for (TE_group in TE_groups){
-    
-    pt = permTest(A = TE_group, 
-                  B = gene_group, 
-                  ntimes = 1000,
-                  randomize.function = resampleRegions,
-                  universe = GRanges_TE,
-                  evaluate.function = meanDistance,
-                  alternative = 'less')
-    
-    p_value = pt$meanDistance[[1]]
-    Z_score = pt$meanDistance[[6]]
-    
-    if (p_value < (0.05/number_of_tests)){
-      
-      output[row_number, column_number] = Z_score
-      
-    }
-    
-    else{
-      
-      output[row_number, column_number] = NA
-      
-    }
-    
-    column_number = column_number + 1
-    
-  }
-  
-  row_number = row_number + 1
-  
-}
