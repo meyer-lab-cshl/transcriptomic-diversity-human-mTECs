ad0003665a98bc3d418772447f4a39822e3b2f4a
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-17 20:36:35 -0400

    position_enrichment_permutation script ready to run

--- analysis/hi_vs_lo/hivslo_genomic_positon_analysis.R
+++ analysis/hi_vs_lo/hivslo_genomic_positon_analysis.R
@@ -116,60 +116,11 @@ ggplot() +
 
 ## Heatmap
 
-#gene_groups = list(GRanges_gene_up, GRanges_gene_unchanged, GRanges_gene_down)
-#TE_groups = list(GRanges_TE_up, GRanges_TE_unchanged, GRanges_TE_down)
-gene_groups = list(GRanges_gene_up, GRanges_gene_down)
-TE_groups = list(GRanges_TE_up, GRanges_TE_down)
+gene_groups = list(GRanges_gene_up, GRanges_gene_unchanged, GRanges_gene_down)
+TE_groups = list(GRanges_TE_up, GRanges_TE_unchanged, GRanges_TE_down)
 
-output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
-rownames(output) = c('gene_up', 'gene_down')
-colnames(output) = c('TE_up', 'TE_down')
-
-row_number = 1
-number_of_tests = ncol(output) + nrow(output)
-
-for (gene_group in gene_groups){
-  
-  column_number = 1
-  
-  for (TE_group in TE_groups){
-    
-    pt = permTest(A = TE_group, 
-                 B = gene_group, 
-                 ntimes = 1000,
-                 randomize.function = resampleRegions,
-                 universe = GRanges_TE,
-                 evaluate.function = meanDistance,
-                 alternative = 'less')
-    
-    p_value = pt$meanDistance[[1]]
-    Z_score = pt$meanDistance[[6]]
-    
-    if (p_value < (0.05/number_of_tests)){
-      
-        output[row_number, column_number] = Z_score
-        
-      }
-  
-    else{
-      
-      output[row_number, column_number] = NA
-      
-    }
-    
-    column_number = column_number + 1
-    
-  }
-  
-  row_number = row_number + 1
-  
-}
-
-
-# Save variables (export them)
-saveRDS(gene_groups, "~/TE_thymus/analysis/cluster_R_stuff/gene_groups.rds")
-
-
+saveRDS(gene_groups, "~/TE_thymus/analysis/cluster/gene_groups.rds")
+saveRDS(TE_groups, "~/TE_thymus/analysis/cluster/TE_groups.rds")
 
 my_heatmap = pheatmap(mat = output, 
                       cluster_rows=FALSE,
@@ -189,3 +140,50 @@ save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
 }
 
 save_pheatmap_png(my_heatmap, "/Users/mpeacey/TE_thymus/analysis/Plots/TE_local/hi_vs_lo_genomic_poisitons_heatmap.png")
+
+
+#####
+
+output = matrix(, nrow = length(gene_groups), ncol = length(TE_groups))
+rownames(output) = c('gene_up', 'gene_down')
+colnames(output) = c('TE_up', 'TE_down')
+
+row_number = 1
+number_of_tests = ncol(output) + nrow(output)
+
+for (gene_group in gene_groups){
+  
+  column_number = 1
+  
+  for (TE_group in TE_groups){
+    
+    pt = permTest(A = TE_group, 
+                  B = gene_group, 
+                  ntimes = 1000,
+                  randomize.function = resampleRegions,
+                  universe = GRanges_TE,
+                  evaluate.function = meanDistance,
+                  alternative = 'less')
+    
+    p_value = pt$meanDistance[[1]]
+    Z_score = pt$meanDistance[[6]]
+    
+    if (p_value < (0.05/number_of_tests)){
+      
+      output[row_number, column_number] = Z_score
+      
+    }
+    
+    else{
+      
+      output[row_number, column_number] = NA
+      
+    }
+    
+    column_number = column_number + 1
+    
+  }
+  
+  row_number = row_number + 1
+  
+}
