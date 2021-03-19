a82ea1bc55ad5c6a50f14c1951be60a6153b557c
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-03-19 16:07:09 -0400

    count_overlaps.R now counts overlaps with down regulated genes

--- analysis/cluster/count_overlaps/count_overlaps.R
+++ analysis/cluster/count_overlaps/count_overlaps.R
@@ -3,9 +3,11 @@
 library(GenomicRanges)
 
 GRanges_gene_up = readRDS("/grid/meyer/home/mpeacey/TE_thymus/analysis/cluster/objects/GRanges_gene_up.rds")
+GRanges_gene_down = readRDS("/grid/meyer/home/mpeacey/TE_thymus/analysis/cluster/objects/GRanges_gene_down.rds")
 GRanges_TE = readRDS("/grid/meyer/home/mpeacey/TE_thymus/analysis/cluster/objects/GRanges_TE.rds")
 
 overlap_with_up_gene = rep(NA, length(GRanges_TE))
+overlap_with_down_gene = rep(NA, length(GRanges_TE))
 
 for (i in 1:length(GRanges_TE)){
   
@@ -23,6 +25,20 @@ for (i in 1:length(GRanges_TE)){
     
   }
   
+  down_hit = countOverlaps(query = GRanges_TE[i], subject = GRanges_gene_down)
+  if (length(up_hit) > 0){
+    
+    overlap_with_down_gene[i] = T
+    
+  }
+  
+  else{
+    
+    overlap_with_down_gene[i] = F
+    
+  }
+  
 }
 
 saveRDS(overlap_with_up_gene, "/grid/meyer/home/mpeacey/TE_thymus/analysis/cluster/count_overlaps/overlap_with_up_gene.rds")
+saveRDS(overlap_with_down_gene, "/grid/meyer/home/mpeacey/TE_thymus/analysis/cluster/count_overlaps/overlap_with_down_gene.rds")
