fc2bd997fabe95c3cddabe4b3b07f24531060805
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-23 21:03:25 -0400

    Updated optimization script for TE local with new TE annotation

--- scripts/TE_local/TE_local_STAR_optimization.sh
+++ scripts/TE_local/TE_local_STAR_optimization.sh
@@ -2,18 +2,17 @@
 #$ -cwd
 #$ -pe threads 1
 #$ -l m_mem_free=30G
-#$ -N TE_local
-#$ -o TE_local_output.txt
-#$ -e TE_local_output.txt
+#$ -N TE_local_opt
+#$ -o TE_local_optimization_output.txt
+#$ -e TE_local_optimization_output.txt
 
-cd $TE_HOME/data/STAR_optimization/mTEC-HI/
+cd $TE_HOME/data/STAR_optimization/mTEC-HI/                         
 
-for FILE in *_Aligned.out.bam; do
+for FILE in *pt226*; do
   TElocal \
   -b ${FILE} \
-  --GTF $TE_HOME/index/human.GRCh38.gtf \
-  --TE $TE_HOME/index/hg38_rmsk_TE.gtf.locInd \
+  --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
+  --TE $TE_HOME/index/annotations/TElocal_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf.locInd \
   --project TE_local_${FILE}
 
 done
-
