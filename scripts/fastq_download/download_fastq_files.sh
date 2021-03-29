fc39fe267efcf2f027e25e3958aa52093e7bea8c
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-29 12:08:47 -0400

    Modified fastq download script

--- scripts/fastq_download/download_fastq_files.sh
+++ scripts/fastq_download/download_fastq_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe threads 4
-#$ -l m_mem_free=3G
+#$ -l m_mem_free=10G
 #$ -N download_fastq_files
 #$ -o download_fastq_files_output.txt
 #$ -e download_fastq_files_output.txt
