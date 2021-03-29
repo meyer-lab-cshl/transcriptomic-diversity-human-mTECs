dcc4a0a648b15e0e9640c73a034b6e1764998eee
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-29 15:55:16 -0400

    Added FastP quality control step before alignment

--- scripts/align_read.sh
+++ scripts/align_read.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #$ -cwd
-#$ -pe threads 12
+#$ -pe threads 4
 #$ -l m_mem_free=32G
 #$ -N align_reads
 #$ -o align_reads_output.txt
 #$ -e align_reads_output.txt
 
-cd $TE_HOME/data/FastP
+tissue=testis
 
-## Generate arrays to hold paired reads
+cd $TE_HOME/data/RNA_seq/${tissue}/raw_fastq/
 
 declare -a read_1_array=()
 declare -a read_2_array=()
@@ -21,22 +21,31 @@ for READ_2 in *_2.fastq; do
   read_2_array+=($READ_2)
 done
 
-## Iterate through each array and run STAR on each pair of reads
+## Iterate through each array and run fastp/STAR on each pair of reads
 
 counter=0
 for i in ${read_1_array[@]}; do
 
+  fastp \
+  -i $TE_HOME/data/RNA_seq/${tissue}/raw_fastq/${i} \
+  -I $TE_HOME/data/RNA_seq/${tissue}/raw_fastq/${read_2_array[$counter]} \
+  -o $TE_HOME/data/RNA_seq/${tissue}/fastp/${i}.fastp \
+  -O $TE_HOME/data/RNA_seq/${tissue}/fastp/${read_2_array[$counter]}.fastp \
+  -q 25 -u 10 -l 50 -y -x -w 4
+  
   STAR \
-  --runThreadN 12 \
+  --runThreadN 4 \
   --genomeDir $TE_HOME/index/ \
-  --sjdbGTFfile $TE_HOME/index/human.GRCh38.gtf \
+  --sjdbGTFfile $TE_HOME/index/annotations/human.GRCh38.gtf \
   --sjdbOverhang 100 \
-  --readFilesIn $i ${read_2_array[$counter]}\
+  --readFilesIn $TE_HOME/data/RNA_seq/${tissue}/fastp/${i}.fastp \
+  $TE_HOME/data/RNA_seq/${tissue}/fastp/${read_2_array[$counter]}.fastp \
   --outSAMtype BAM Unsorted \
   --winAnchorMultimapNmax 200 \
   --outFilterMultimapNmax 100 \
-  --outFileNamePrefix $TE_HOME/data/${i}_
+  --outFileNamePrefix $TE_HOME/data/RNA_seq/${tissue}/bam_files/${i}_
   
   counter=$((counter+1))
 
 done
+
