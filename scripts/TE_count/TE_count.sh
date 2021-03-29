de98b063bc057fcbdb3911e7e38de207b0895533
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-29 18:51:09 -0400

    Edited TEcount script to iterate through BAM files

--- scripts/TE_count/TE_count.sh
+++ scripts/TE_count/TE_count.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe threads 1
-#$ -l m_mem_free=8G
+#$ -l m_mem_free=16G
 #$ -N TE_count
 #$ -o TE_count_output.txt
 #$ -e TE_count_output.txt
 
-cd $TE_HOME/data
+tissue=testis_jason
 
-TEcount \
--b pt214_hi_fastp_1.fastq_Aligned.out.bam \
---GTF $TE_HOME/index/human.GRCh38.gtf \
---TE $TE_HOME/index/GRCh38_GENCODE_rmsk_TE.gtf \
---project TE_count_test
+cd $TE_HOME/data/RNA_seq/${tissue}/test
 
+counter=0
+for FILE in GTEX*; do
+
+  TEcount \
+  -b ${FILE} \
+  --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
+  --TE $TE_HOME/index/annotations/TEtranscripts_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf \
+  --project TE_count_${FILE}
+
+  if [${counter} -eq 0]
+  then
+    TE_count_${FILE} > count_table
+  else
+    join count_table TE_count_${FILE} > count_table
+  fi
+
+  counter=$((counter+1))
+
+done
