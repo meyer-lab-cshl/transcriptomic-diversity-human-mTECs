b78ad5a9cb7ca792c53c29efd5e6c5c51da20b1a
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-03-30 14:28:29 -0400

    No change

--- scripts/TE_count/TE_count.sh
+++ scripts/TE_count/TE_count.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe threads 1
-#$ -l m_mem_free=16G
+#$ -l m_mem_free=32G
 #$ -N TE_count
 #$ -o TE_count_output.txt
 #$ -e TE_count_output.txt
 
-tissue=testis_jason
+tissue=ovaries_jason
 
 cd $TE_HOME/data/RNA_seq/${tissue}/test
 
-counter=0
 for FILE in GTEX*; do
 
   TEcount \
@@ -18,14 +17,5 @@ for FILE in GTEX*; do
   --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
   --TE $TE_HOME/index/annotations/TEtranscripts_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf \
   --project TE_count_${FILE}
-
-  if [${counter} -eq 0]
-  then
-    TE_count_${FILE} > count_table
-  else
-    join count_table TE_count_${FILE} > count_table
-  fi
-
-  counter=$((counter+1))
 
 done
