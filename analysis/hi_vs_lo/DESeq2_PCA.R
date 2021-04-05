d1e542cfa3c78fff68ebc0311a17002a337fef29
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-04-04 21:37:04 -0400

    Updated with new ovary samples

--- analysis/hi_vs_lo/DESeq2_PCA.R
+++ analysis/hi_vs_lo/DESeq2_PCA.R
@@ -5,8 +5,8 @@ library(DESeq2)
 # PCA
 #################################################################
 
-PCA = plotPCA(vs_dds_local_TE, intgroup = 'tissue') + 
-  ggtitle('TE expression PCA', 'TElocal') 
+PCA = plotPCA(vs_dds_transcripts_TE, intgroup = 'tissue') + 
+  ggtitle('TE expression PCA', 'TEtranscripts') 
 
 PCA + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 20),
                          plot.subtitle = element_text(size = 14),
@@ -16,5 +16,5 @@ PCA + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 20),
                          axis.line = element_line(size = 0.8),
                          panel.border = element_blank())
 
-ggsave("/Users/mpeacey/TE_thymus/analysis/Plots/21-04-01/PCA.png", 
-       width = 20, height = 20, units = "cm")
+ggsave("/Users/mpeacey/TE_thymus/analysis/Plots/Presentation/transcripts_PCA.png", 
+       width = 7, height = 6, units = "in")
