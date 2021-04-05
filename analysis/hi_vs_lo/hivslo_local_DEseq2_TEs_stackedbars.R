c81fedeffc9ff6eb6867c28099e12012447aa187
Matthew Peacey <mpeacey@matty-p.cshl.edu>
2021-04-05 19:55:34 -0400

    Updated plot aesthetics

--- analysis/hi_vs_lo/hivslo_local_DEseq2_TEs_stackedbars.R
+++ analysis/hi_vs_lo/hivslo_local_DEseq2_TEs_stackedbars.R
@@ -16,9 +16,9 @@ build_count_table = function(dds, results_df, group, mode, by){
   ## Merge with results_df, determine mean and remove unnecessary columns
   
   normalized_counts = merge(normalized_counts, results_df, by = 'ID')
-  normalized_counts = select(normalized_counts, -c('214_LO', '221_LO', '226_LO'))
+  normalized_counts = select(normalized_counts, -c('pt214_mTEC-lo_new', 'pt221_mTEC-lo_new', 'pt226_mTEC-lo_new'))
   normalized_counts$mean = rowMeans(normalized_counts[2:4])
-  normalized_counts = select(normalized_counts, -c('214_HI', '221_HI', '226_HI'))
+  normalized_counts = select(normalized_counts, -c('pt214_mTEC-hi_new', 'pt221_mTEC-hi_new', 'pt226_mTEC-hi_new'))
   
   normalized_counts = mutate(normalized_counts, class = sub("\\?", "", class))
   
@@ -205,18 +205,25 @@ build_count_table = function(group){
   
 }
 
-count_table = build_count_table(group = c('all', 'diff_regulated', 'up_regulated')) %>% 
-  filter(percent > 1)
+count_table = build_count_table(dds_local_TE, 
+                                results_df_local_TE, 
+                                group = c('all', 'down_regulated', 'up_regulated'),
+                                mode = 'class',
+                                by = 'normalized_reads')
 
 ## Plot stacked bars
 
-bar_chart = ggplot(count_table, aes(x = group, y = percent, fill = Class)) + 
+bar_chart = ggplot(count_table, aes(x = group, y = percent, fill = class)) + 
   geom_col(colour = 'black', position = 'fill') +
   scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, .1))) +
-  scale_fill_brewer(palette = "Set1") +
-  xlab('') +
+  scale_fill_brewer(palette = 'Set1') +
+  xlab('mTEC-HI subset') +
   ylab('Fraction of normalized reads') +
-  ggtitle('TEs in mTEC-HI cells', 'Subset by physical overlap with genes')
+  labs(fill= "Class") +
+  scale_x_discrete(labels = c('All', 'Down', 'Up'))
+
+  
+  #ggtitle('TEs in mTEC-HI cells', 'Subset by physical overlap with genes')
 
 bar_chart + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 20),
                                plot.subtitle = element_text(size = 14),
@@ -224,13 +231,42 @@ bar_chart + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 2
                                panel.grid.minor.x = element_blank(),
                                panel.grid.major.y = element_line(color = 'grey'),
                                panel.grid.minor.y = element_blank(),
-                               axis.text.x = element_text(size = 13),
+                               axis.text.x = element_text(size = 13, margin = margin(t = 6)),
                                axis.text.y = element_text(size = 14),
-                               axis.title = element_text(size = 14),
+                               axis.title.y = element_text(size = 14),
+                               axis.title.x = element_text(size = 14, margin = margin(t = 6)),
                                axis.line = element_line(size = 0.8),
                                panel.border = element_blank(),
                                legend.text = element_text(size = 12),
                                legend.title = element_text(size = 14))
 
-ggsave("/Users/mpeacey/TE_thymus/analysis/Plots/21-03-25/slide_3.png", 
-       width = 20, height = 13, units = "cm")
+ggsave("/Users/mpeacey/TE_thymus/analysis/Plots/Presentation/local_class_distribution.png", 
+       width = 8, height = 4.5, units = "in")
+
+
+
+## Chi-square
+
+contingency = pivot_wider(count_table, id_cols = class, names_from = group, values_from = percent) %>% 
+  mutate_if(is.numeric, list(~replace_na(., 0))) %>% 
+  as.data.frame() %>%
+  pivot_longer(cols = c(all, down_regulated, up_regulated), names_to = 'group', values_to = 'count')
+
+contingency_table = xtabs(count ~ group+class, data=contingency)
+
+vcd::mosaic(~group+class, data = contingency_table, direction = c('v', 'h'), shade = T)
+
+## Chi-square v2
+
+contingency = pivot_wider(count_table, id_cols = class, names_from = group, values_from = percent) %>% 
+  mutate_if(is.numeric, list(~replace_na(., 0))) %>% 
+  as.data.frame()
+
+rownames(contingency) = contingency$class
+contingency = dplyr::select(contingency, -class)
+
+chisq.test(contingency)
+
+chisq.posthoc.test::chisq.posthoc.test(contingency)
+
+
