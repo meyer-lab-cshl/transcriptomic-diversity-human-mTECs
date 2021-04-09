1dd126fd7a6d93a4fbde0e7f496ebbb3dec78fcc
Matthew Peacey <mpeacey@Matty-P.local>
2021-04-09 15:42:11 -0400

    Renamed folders

---
+++ cluster_scripts/fastq_download/download_fastq_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe threads 4
+#$ -l m_mem_free=10G
+#$ -N download_fastq_files
+#$ -o download_fastq_files_output.txt
+#$ -e download_fastq_files_output.txt
+
+while read p; do
+  fasterq-dump "$p" -p -e 15
+done <acc.txt
