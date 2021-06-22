4473d892b276c8f47da000663b69b3b847eb8ee4
HannahVMeyer <hannah.v.meyer@googlemail.com>
2021-06-22 10:03:12 -0400

    Remove redundant mouse analysis

--- mouse/make_dag_tss.sh
+++
@@ -1,3 +0,0 @@
-snakemake -s tss.smk \
-    --rulegraph ~/data/tss/mouse/5Pseq/tss/summary/all_samples_consensus_sites.csv \
-    ~/data/tss/mouse/5Pseq/tss/isoforms/mESC1_1.isoforms.csv |  dot -Tpng > dag/tss_dag.png
