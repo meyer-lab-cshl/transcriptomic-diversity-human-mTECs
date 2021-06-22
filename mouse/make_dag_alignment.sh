238ea955822fbc21de2c468bfb575b3c5c5aa83c
HannahVMeyer <hannah.v.meyer@googlemail.com>
2021-06-22 10:03:12 -0400

    Remove redundant mouse analysis

--- mouse/make_dag_alignment.sh
+++
@@ -1,4 +0,0 @@
-snakemake -s alignment.smk \
-    --rulegraph ~/data/tss/mouse/5Pseq/alignments/mESC1_1_Aligned.sortedByCoord.out.bam \
-    ~/data/tss/mouse/5Pseq/multiqc/multiqc_report.html \
-    ~/data/tss/mouse/5Pseq/alignment_qc/multiqc_report.html | dot -Tpng > dag/alignment_dag.png
