071753497b0ff772e01d8f8ac62d1b1a448a29c6
HannahVMeyer <hannah.v.meyer@googlemail.com>
2021-06-22 10:12:42 -0400

    TSS calling now via paraclu

--- human/make_dag_tss.sh
+++
@@ -1,3 +0,0 @@
-snakemake -s tss.smk \
-    --rulegraph ~/data/tss/human/5Pseq/tss/summary/all_samples_consensus_sites.csv \
-    ~/data/tss/human/5Pseq/tss/isoforms/pt212-hi.isoforms.csv |  dot -Tpng > dag/tss_dag.png
