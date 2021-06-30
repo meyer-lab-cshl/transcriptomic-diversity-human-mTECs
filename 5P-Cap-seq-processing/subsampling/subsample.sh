0d080b64496ad1eb7a76b3cbfd9286aef22eed31
Hannah Meyer <hannah.v.meyer@gmail.com>
2021-06-30 08:45:16 -0400

    generare and extract proportions from subsampling matrix

--- 5P-Cap-seq-processing/subsampling/subsample.sh
+++ 5P-Cap-seq-processing/subsampling/subsample.sh
@@ -28,15 +28,8 @@ while getopts c:s:r:t:i:o: opt; do
   esac
 done
 
-#FACTOR=$(samtools idxstats $1 | cut -f3 | awk -v COUNT=$2 'BEGIN {total=0} {total += $1} END {print COUNT/total}')
-
-echo "awk -v SAMPLE=$sample -v READS=$reads '(\$1 == SAMPLE) {print READS/\$2}' $counts"
-FACTOR=$(awk -v SAMPLE=$sample -v READS=$reads '($1 == SAMPLE) {print READS/$2}' $counts)
-
-#if [[ $FACTOR > 1 ]]
-#  then
-#  echo '[ERROR]: Requested number of reads exceeds total read count in' $1 '-- exiting' && exit 1
-#fi
+echo "awk -v SAMPLE=$sample -v READS=$reads '(\$1 == SAMPLE && \$2 == READS) {print $4}' $counts"
+FACTOR=$(awk -v SAMPLE=$sample -v READS=$reads '($1 == SAMPLE && $2 == READS) {print $4}' $counts)
 
 echo "sambamba view -s $FACTOR -t $threads --subsampling-seed=$reads -f bam -l 5 -o $output $input"
 sambamba view -s $FACTOR -t $threads --subsampling-seed=$reads -f bam -l 5 -o $output $input
