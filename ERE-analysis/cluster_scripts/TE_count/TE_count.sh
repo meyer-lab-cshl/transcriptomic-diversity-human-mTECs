bc34cd94a9eaed1be8140be14e865a6eef7c171b
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-07-07 10:19:41 -0400

    Generalized TE_count script for all tissues

--- ERE-analysis/cluster_scripts/TE_count/TE_count.sh
+++ ERE-analysis/cluster_scripts/TE_count/TE_count.sh
@@ -2,20 +2,27 @@
 #$ -cwd
 #$ -pe threads 1
 #$ -l m_mem_free=32G
-#$ -N TE_count_O
+#$ -N TE_count
 #$ -o TE_count_output.txt
 #$ -e TE_count_output.txt
 
-tissue=ovaries_jason
+TE_HOME=/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis
+GTEX=/grid/meyer/home/jacarter/GTEx/Aligned_TE
 
-cd $TE_HOME/data/RNA_seq/${tissue}/
+cd $GTEX/
 
-for FILE in GTEX*; do
+for TISSUE in *; do
 
-  TEcount \
-  -b ${FILE} \
-  --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
-  --TE $TE_HOME/index/annotations/TEtranscripts_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf \
-  --project TE_count_${FILE}
+  cd ${TISSUE}
+
+  for FILE in GTEX*; do
+
+    TEcount \
+    -b ${FILE} \
+    --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
+    --TE $TE_HOME/index/annotations/TEtranscripts_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf \
+    --project TEcount_~{TISSUE}_${FILE}
+
+  done
 
 done
