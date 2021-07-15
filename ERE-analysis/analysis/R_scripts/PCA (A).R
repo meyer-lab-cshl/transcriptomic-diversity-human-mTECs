dd37299658545b91b3bf60ccfcf7d330261c3785
Matthew Peacey <mpeacey@neverwhere.cshl.edu>
2021-07-15 14:31:01 -0400

    Fraction TEs: colored by tissue

--- ERE-analysis/analysis/R_scripts/PCA (A).R
+++ ERE-analysis/analysis/R_scripts/PCA (A).R
@@ -8,7 +8,7 @@ functions = c('extract_subset', 'differential_expression')
 
 for (i in functions){
   
-  load(glue('{functions_directory}{i}'))
+  load(glue::glue('{functions_directory}{i}'))
   
 }
 
@@ -23,11 +23,11 @@ for (i in functions){
 ## Data import:
 
 count_table_directory = "/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/count_tables/"
-data = read.table(glue('{count_table_directory}TE_transcripts_counts'),header=T,row.names=1)
+transcripts_data = read.table(glue::glue('{count_table_directory}TE_transcripts_counts'),header=T,row.names=1)
 
 ## Run DESeq2 to obtain normalized counts
 
-TE_data = extract_subset(mode = 'TE', input = data)
+TE_data = extract_subset(mode = 'TE', input = transcripts_data)
 
 dds_transcripts_TE = differential_expression(TE_data, design=~tissue)
 
@@ -47,15 +47,15 @@ percentVar = round(100 * attr(pcaData, "percentVar"))
 ## Use this to have only a subset of tissues in color: useful when you have lots of tissues
 ## and you're only interested in a few.
 
-colored_tissues = c('mTEC.hi', 'mTEC.lo', 'ESC', 'Testis', 'Brain.Caudate..basal.ganglia.')
+colored_tissues = c('mTEC.hi', 'mTEC.lo', 'ESC', 'Testis')
 
 pcaData = mutate(pcaData, color = case_when(tissue %in% colored_tissues ~ T,
                                    !(tissue %in% colored_tissues) ~ F))
 
 PCA = ggplot(pcaData, aes(PC1, PC2)) + 
-  geom_point(data = pcaData, aes(x = PC1, y = PC2), color = '#9A9A9A', size = 2) +
-  geom_point(data = subset(pcaData, color == F), size=4, shape = 21, stroke = 0) +
-  geom_point(data = subset(pcaData, color == T), aes(fill = tissue), size=4, shape = 21, stroke = 0) +
+  geom_point(data = pcaData, aes(x = PC1, y = PC2), color = '#9A9A9A', size = 3) +
+  geom_point(data = subset(pcaData, color == F), size=3, shape = 21, stroke = 0) +
+  geom_point(data = subset(pcaData, color == T), aes(fill = tissue), size=3, shape = 21, stroke = 0) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
   coord_fixed() + 
@@ -81,3 +81,51 @@ ggsave("/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/Plot
 #################################################################
 # Fraction of reads mapping to TEs (supplement A?)
 #################################################################
+
+dds_transcripts = differential_expression(transcripts_data, design=~tissue)
+
+df_gene_counts = data.frame(gene_counts = colSums(counts(extract_subset(mode = 'gene', input = dds_transcripts), normalized = T)))
+df_gene_counts = cbind(ID = rownames(df_gene_counts), df_gene_counts)
+
+df_TE_counts = data.frame(TE_counts = colSums(counts(extract_subset(mode = 'TE', input = dds_transcripts), normalized = T)))
+df_TE_counts = cbind(ID = rownames(df_TE_counts), df_TE_counts)
+
+df = merge(df_gene_counts, df_TE_counts, by = 'ID')
+
+df = mutate(df, total = gene_counts + TE_counts)
+df = mutate(df, percent_TEs = (TE_counts / total)*100)
+
+df = separate(df, col = ID, into = c('patient', 'tissue'), sep = '_')
+
+df = mutate(df, tissue = forcats::fct_reorder(tissue, percent_TEs))
+
+## Boxplot
+
+df = mutate(df, color = case_when(tissue %in% colored_tissues ~ T,
+                                      !(tissue %in% colored_tissues) ~ F))
+
+plot = ggplot(data = df, aes(x = tissue, y = percent_TEs)) +
+  geom_boxplot(data = subset(df, color == F), fill = '#9A9A9A', width = 0.6) +
+  geom_boxplot(data = subset(df, color == T), aes(fill = tissue), width = 0.6) +
+  geom_point() +
+  ylab('% of reads mapping to TEs') +
+  xlab('Tissue/cell') +
+  scale_fill_manual(values = c('#4c72b0ff', '#dd8452ff', '#55A257', '#E93C00'))
+
+plot + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 16),
+                          plot.subtitle = element_text(size = 12),
+                          panel.grid.major.x = element_blank(),
+                          panel.grid.minor.x = element_blank(),
+                          panel.grid.major.y = element_blank(),
+                          panel.grid.minor.y = element_line(color = 'gray'),
+                          axis.text.x = element_blank(),
+                          axis.text.y = element_text(size = 13),
+                          axis.title.x = element_text(size = 15, margin = margin(t = 12.5)),
+                          axis.title.y = element_text(size = 15, margin = margin(r = 7.5)),
+                          axis.line = element_line(size = 0.8),
+                          panel.border = element_blank(),
+                          legend.text = element_text(size = 12),
+                          legend.title = element_text(size = 14))
+
+ggsave("~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/Plots/overall_TE_abundance.png", 
+       width = 7, height = 5, units = "in")
