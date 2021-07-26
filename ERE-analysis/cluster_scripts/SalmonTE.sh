89deea98a50b226d47d41b1439b3b58e54f548e1
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-07-26 08:20:14 -0400

    SQuIRE and SalmonTE

--- ERE-analysis/cluster_scripts/SalmonTE.sh
+++ ERE-analysis/cluster_scripts/SalmonTE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe threads 1
-#$ -l m_mem_free=2G
+#$ -l m_mem_free=1G
 #$ -N SalmonTE
 #$ -o SalmonTE_output.txt
 #$ -e SalmonTE_output.txt
@@ -10,18 +10,34 @@ echo '########'
 echo 'New run'
 echo '########'
 
+echo $2
 cat $1 | while read line || [[ -n $line ]];do
-
-	SalmonTE.py quant \
-	--reference=hs \
-	--outpath=/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line \
-	--num_threads=4 \
-	/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line/fastp
 	
-	sleep 3h
+#	if [ $2=='raw' ]
+#	then
+#		
+#		echo 'Calculating raw counts for tissue' $line
+#
+#		SalmonTE.py quant \
+ #       	--reference=hs \
+  #      	--outpath=/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line/raw_Salmon_counts/ \
+   #     	--exprtype=count \
+#		--num_threads=4 \
+ #       	/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line/fastp
+#	fi
+	if [ $2=='TPM' ]
+	then
+		
+		echo 'Calculating normalized counts for tissue' $line
+
+		SalmonTE.py quant \
+		--reference=hs \
+		--outpath=/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line \
+		--num_threads=4 \
+		/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis/data/$line/fastp
+	
+	fi
+
+	sleep 20m
 
 done
-
-  
-  
-
