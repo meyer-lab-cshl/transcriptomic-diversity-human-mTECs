995f5375c4a0bedb948130328be9a8963457d687
Peacey <mpeacey@bamdev1.elzar.cshl.edu>
2021-07-28 13:15:25 -0400

    TE_local script update

--- ERE-analysis/cluster_scripts/TE_local/TE_local.sh
+++ ERE-analysis/cluster_scripts/TE_local/TE_local.sh
@@ -6,9 +6,10 @@
 #$ -o TE_local_output.txt
 #$ -e TE_local_output.txt
 
-cd $TE_HOME/data/RNA_seq/muscle/bam_files/TE_local_test_set                    
+TE_HOME=/grid/meyer/home/mpeacey/thymus-epitope-mapping/ERE-analysis
+cd $TE_HOME/data/$1/                    
 
-for FILE in *_Aligned.out.bam; do
+for FILE in *Aligned.out.bam; do
   TElocal \
   -b ${FILE} \
   --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
@@ -17,27 +18,6 @@ for FILE in *_Aligned.out.bam; do
 
 done
 
-cd $TE_HOME/data/RNA_seq/testis_jason/TE_local_test_set
-
-for FILE in *_Aligned.out.bam; do
-  TElocal \
-  -b ${FILE} \
-  --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
-  --TE $TE_HOME/index/annotations/TElocal_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf.locInd \
-  --project TE_local_${FILE}
-
-done
-
-cd $TE_HOME/data/RNA_seq/ovaries_jason/TE_local_test_set
-
-for FILE in *_Aligned.out.bam; do
-  TElocal \
-  -b ${FILE} \
-  --GTF $TE_HOME/index/annotations/human.GRCh38.gtf \
-  --TE $TE_HOME/index/annotations/TElocal_prebuilt_indices/GRCh38_GENCODE_rmsk_TE.gtf.locInd \
-  --project TE_local_${FILE}
-
-done
 
 
 
