8ac52d61d93b3001d2e1dafb4f679421ef688010
Matthew Peacey <mpeacey@gonzo4.cshl.edu>
2021-08-24 12:59:23 +0100

    Genomic position analysis script cleanup

--- ERE-analysis/analysis/R_scripts/Genomic_position_analysis.R
+++ ERE-analysis/analysis/R_scripts/Genomic_position_analysis.R
@@ -1,119 +1,119 @@
-library(DESeq2)
+################################################################################
+# Generates GRanges objects and lists of genes/EREs to be used in downstream 
+# analysis. Requires DESeq2 to be run on TElocal output.
+################################################################################
+
+# Load relevant packages
 library(tidyverse)
 library(GenomicRanges)
-library(regioneR)
-library(pheatmap)
-library(RColorBrewer)
 library(glue)
 
-functions_directory = "/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_functions/"
-functions = c('extract_subset', 
-              'differential_expression', 
-              'process_DESeq2_results', 
-              'make_GRanges', 
-              'run_perm_test', 
-              'calculate_odds_ratio_of_overlap',
-              'save_heatmap_png')
-
-for (i in functions){q
-  
-  load(glue('{functions_directory}{i}'))
-  
-}
+# Import required functions
+working_directory = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis'
+functions_directory = glue("{working_directory}/R_functions")
+functions = c('extract_subset',
+              'make_GRanges')
+for (i in functions){load(glue('{functions_directory}/{i}'))}
 
 ## Import required variables
 results_df_local_ERE = readRDS(file = glue('{working_directory}/R_variables/results_df_local_ERE'))
 
-#################################################################
-# Generating GRanges objects
-#################################################################
+################################################################################
+# Genes
+################################################################################
 
-## TEs
-
-GRanges_TE = make_GRanges(mode = 'TE', results_df = results_df_local_TE)
-
-GRanges_TE_start = GRanges_TE
-end(GRanges_TE_start) = start(GRanges_TE_start)
-
-## Genes
-
-gene_annotation = read.table(file = '/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/annotation_tables/gencode.v38_gene_annotation_table.txt', header = 1)
-gene_annotation = dplyr::select(gene_annotation, c('Geneid', 'Chromosome', 'Start', 'End', 'Strand', 'Class'))
-gene_annotation = dplyr::rename(gene_annotation, chr = Chromosome, start = Start, end = End, strand = Strand)
+## Imports a gene annotation
+gene_annotation = read.table(file = glue('{working_directory}/annotation_tables/gencode.v38_gene_annotation_table.txt', 
+                                         header = 1))
+gene_annotation = dplyr::select(gene_annotation, 
+                                c('Geneid', 'Chromosome', 'Start', 'End', 'Strand', 'Class'))
+gene_annotation = dplyr::rename(gene_annotation, 
+                                chr = Chromosome, 
+                                start = Start, 
+                                end = End, 
+                                strand = Strand)
 gene_annotation$Geneid = gsub('\\..+$', '', gene_annotation$Geneid)
+
+## Converts to a GRanges object
 GRanges_gene = makeGRangesFromDataFrame(gene_annotation, keep.extra.columns = T)
+saveRDS(GRanges_gene, file = glue('{working_directory}/R_variables/GRanges_gene'))
 
-saveRDS(GRanges_gene, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/GRanges_gene')
-
-#GRanges_gene = make_GRanges(mode = 'gene', results_df = results_df_local_gene)
-
+## Extends each GRanges entry to include 1 kbp up- and down-stream
 GRanges_gene_extended = GRanges_gene
 end(GRanges_gene_extended) = end(GRanges_gene) + 1000
 start(GRanges_gene_extended) = start(GRanges_gene) - 1000
+saveRDS(GRanges_gene_extended, file = glue('{working_directory}/R_variables/GRanges_gene_extended'))
 
-saveRDS(GRanges_gene_extended, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/GRanges_gene_extended')
+# Genes of interest
+AIRE_genes = read.csv(file = glue('{working_directory}/gene_lists/human_aire_dep_genes_san.csv'))
+FEZF2_genes = read.csv(file = glue('{working_directory}/gene_lists/human_fezf2_dep_genes.csv'))
+TRA_genes = read.csv(file = glue('{working_directory}/gene_lists/tra_genes.csv'))
+housekeeping_genes = read.csv(file = glue('{working_directory}/gene_lists/housekeeping_genes.csv'))
 
-#GRanges_ERE = make_GRanges(mode = 'TE', results_df = results_df_local_ERE)
+################################################################################
+# EREs
+################################################################################
 
-annotation = read.table(file = "/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/annotation_tables/hg38_rmsk_TE.gtf.locInd.locations.txt", header = 1)
+annotation = read.table(file = glue("{working_directory}/annotation_tables/hg38_rmsk_TE.gtf.locInd.locations.txt"), 
+                        header = 1)
 annotation = separate(annotation, chromosome.start.stop, into = c('chr', 'start.stop'), sep = ':')
 annotation = separate(annotation, start.stop, into = c('start', 'end'), sep = '-')
 annotation = dplyr::rename(annotation, locus = TE)
 
-count_table_directory = glue::glue("{working_directory}/count_tables/TE_local")
-list_of_EREs = extract_subset(read.table(glue::glue('{count_table_directory}/TE_local_hi_vs_lo.cntTable'),header=T,row.names=1), mode = 'ERE')
+count_table_directory = glue("{working_directory}/count_tables/TE_local")
+list_of_EREs = extract_subset(read.table(glue::glue('{count_table_directory}/TE_local_hi_vs_lo.cntTable'),
+                                         header=T,row.names=1), 
+                              mode = 'ERE')
 list_of_EREs$ID = row.names(list_of_EREs)
 list_of_EREs = separate(list_of_EREs, col = 'ID', into = 'locus', sep = ':')$locus
 
 ERE_annotation = subset(annotation, locus %in% list_of_EREs)
 
 GRanges_ERE = makeGRangesFromDataFrame(ERE_annotation, keep.extra.columns = T)
-saveRDS(GRanges_ERE, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/GRanges_ERE')
+saveRDS(GRanges_ERE, file = glue('{working_diretory}/R_variables/GRanges_ERE'))
 
 GRanges_ERE_start = GRanges_ERE
 end(GRanges_ERE_start) = start(GRanges_ERE_start)
 
-saveRDS(GRanges_ERE_start, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/GRanges_ERE_start')
+saveRDS(GRanges_ERE_start, file = glue('{working_diretory}/R_variables/GRanges_ERE_start'))
 
 GRanges_detected_ERE_start = GRanges_detected_EREs
 end(GRanges_detected_ERE_start) = start(GRanges_detected_EREs)
-
-# Genes of interest
-
-AIRE_genes = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/human_aire_dep_genes_san.csv')
-FEZF2_genes = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/human_fezf2_dep_genes.csv')
-TRA_genes = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/tra_genes.csv')
-housekeeping_genes = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/housekeeping_genes.csv')
-
-up_genes = subset(results_df_local_gene, significant == T & log2FoldChange > 0)$Geneid
-down_genes = subset(results_df_local_gene, significant == T & log2FoldChange < 0)$Geneid
 
 # TEs of interest
 
 up_EREs = subset(results_df_local_ERE, significant == T & log2FoldChange > 0)$locus
 down_EREs = subset(results_df_local_ERE, significant == T & log2FoldChange < 0)$locus
 
-#################################################################
+################################################################################
 # Gene differential expression (from Jason)
-#################################################################
+################################################################################
 
-Sleuth_results_sig = read.csv('~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/Sleuth_results_sig.txt')
+Sleuth_results_sig = read.csv(glue('{working_directory}/R_variables/Sleuth_results_sig.txt'))
 
 up_genes = subset(Sleuth_results_sig, b < 0)$ens_gene
 down_genes = subset(Sleuth_results_sig, b > 0)$ens_gene
 
-saveRDS(up_genes, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/up_genes')
-saveRDS(down_genes, file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/R_variables/down_genes')
+saveRDS(up_genes, file = glue('{working_directory}/R_variables/up_genes'))
+saveRDS(down_genes, file = glue('{working_directory}/R_variables/down_genes'))
 
-#################################################################
+################################################################################
 # ereMAPs
-#################################################################
+#
+# Takes the list of ereMAPs from Larouche et al (supplmentary file ...) and 
+# detects overlap events with EREs to generate a list of ereMAP loci.
+################################################################################
 
-ereMAPs = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/ereMAPs_Larouche.csv', header = T)
+## Import the list of ereMAPS
+
+ereMAPs = read.csv(file = glue('{working_directory}/gene_lists/ereMAPs_Larouche.csv'), 
+                   header = T)
 ereMAPs$Start <- as.numeric(gsub(",","",ereMAPs$Start))
 ereMAPs$End <- as.numeric(gsub(",","",ereMAPs$End))
 
 GRanges_ereMAPs = makeGRangesFromDataFrame(ereMAPs, keep.extra.columns = T)
+
+## Cross reference with ERE locations
 
 GRanges_detected_EREs = make_GRanges(results_df = results_df_local_ERE, mode = 'TE')
 
@@ -144,6 +144,10 @@ for (entry in 1:nrow(TE_ereMAP_overlaps)){
   ereMAP_loci[entry] = GRanges_detected_EREs[TE_hit, ]$ID
   
 }
+
+saveRDS(ereMAP_loci, file = glue('{working_directory}/R_variables/ereMAP_loci'))
+
+## Contingency table
 
 results_df_local_ERE = mutate(results_df_local_ERE, ereMAP = case_when(ID %in% ereMAP_loci ~ T,
                                                                       !(ID %in% ereMAP_loci) ~ F))
