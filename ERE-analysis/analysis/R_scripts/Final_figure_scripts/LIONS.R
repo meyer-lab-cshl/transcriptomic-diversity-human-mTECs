0ae494097a6b194776c51ebb629b8b601fdbde57
Matthew Peacey <mpeacey@Matty-P.local>
2022-04-15 08:23:23 -0400

    Updated LIONS script

--- ERE-analysis/analysis/R_scripts/Final_figure_scripts/LIONS.R
+++ ERE-analysis/analysis/R_scripts/Final_figure_scripts/LIONS.R
@@ -9,27 +9,6 @@ working_directory = '/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/
 #  Data import
 #################################################################
 
-## From final LIONS output
-
-LIONS_HI = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/External_packages/LIONS/mTEC-analysis/mTEC-analysis.rslions', 
-         sep = '\t', header = T)
-LIONS_HI['LO_occurence'] = LIONS_HI['Normal_Occ']
-LIONS_HI['HI_occurence'] = LIONS_HI['Cancer_Occ']
-LIONS_HI = dplyr::select(LIONS_HI, -c('Normal_Occ', 'Cancer_Occ')) %>%
-  mutate(classification = 'HI')
-
-LIONS_LO = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/External_packages/LIONS/mTEC-analysis/mTEC-analysis.inv.rslions', 
-                    sep = '\t', header = T)
-LIONS_LO['LO_occurence'] = LIONS_LO['Cancer_Occ']
-LIONS_LO['HI_occurence'] = LIONS_LO['Normal_Occ']
-LIONS_LO = dplyr::select(LIONS_LO, -c('Normal_Occ', 'Cancer_Occ')) %>%
-  mutate(classification = 'LO')
-
-LIONS = bind_rows(LIONS_HI, LIONS_LO) %>%
-  separate(coordinates, into = c('chromosome', 'range'), sep = ':') %>%
-  separate(range, into = c('start', 'end'), sep = '-') %>%
-  separate(repeatName, remove = F, into = c('sub-family', 'class', 'family'), sep = ':')
-
 ## From Chimera 
 
 Chimera = read.csv(file = '~/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/External_packages/LIONS/mTEC-analysis/mTEC-analysis.chimera', 
@@ -44,6 +23,13 @@ Chimera = mutate(Chimera, total_occurence = LO_occurence + HI_occurence) %>%
 ## Remove non EREs
 
 Chimera = Chimera[Chimera$class != 'DNA', ]
+
+## Remove un-needed columns
+
+Chimera = Chimera[, c('repeatName', 'gene', 'class', 'family', 
+                      'coordinates', 'RStrand', 'Contribution', 'RepeatID', 
+                      'LIBRARY', 'Geneid', 'Gene_strand', 'Gene_type', 
+                      'LO_occurence', 'HI_occurence', 'total_occurence')]
 
 ## Filtering Chimeric-gene transcription events
 
@@ -97,37 +83,29 @@ for (entry in 1:nrow(sense_Chimera)){
 sense_Chimera = subset(sense_Chimera, Gene_strand != '')
 Chimera = sense_Chimera  
 
-#################################################################
-#  Filtering by expression
-#################################################################
-
-## Strict filtering
-Filtered_Chimera = subset(Chimera, Group == 2) %>%
-  subset(LO_occurence <= 1 & HI_occurence >= 2) %>%
-  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
-
-## Not-so-strict 
-Filtered_Chimera = subset(Chimera, Group == 2) %>%
-  subset(LO_occurence <= 0 & HI_occurence >= 1) %>%
-  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
+################################################################################
+# Filtering by expression
+################################################################################
 
 ## Filter for total occurrence 
 Filtered_Chimera = subset(Chimera, total_occurence >= 2) %>%
+  mutate(HI_specific = case_when(LO_occurence <= 1 & HI_occurence >= 2 ~ T,
+                                 T ~ F)) %>%
   mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
 
 ## Filter for induction detected by TElocal + DEseq2
-results_df_local_ERE = readRDS(file = glue::glue('{working_directory}/R_variables/results_df_local_ERE'))
-up_TEs = subset(results_df_local_TE, significant == T & log2FoldChange > 0)$locus
-Filtered_Chimera = subset(Chimera, TEid %in% up_TEs) %>%
-  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
+
+#results_df_local_ERE = readRDS(file = glue::glue('{working_directory}/R_variables/results_df_local_ERE'))
+#up_TEs = subset(results_df_local_TE, significant == T & log2FoldChange > 0)$locus
+#Filtered_Chimera = subset(Chimera, TEid %in% up_TEs) %>%
+#  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
 
 ## Filter for induction detected by TElocal + edgeR
 
-edgeR_results_local_ERE = readRDS(file = glue::glue('{working_directory}/R_variables/edgeR_results_local_ERE'))
-up_TEs = subset(edgeR_results_local_ERE, significant == T & logFC > 0)$locus
-Filtered_Chimera = subset(Chimera, TEid %in% up_TEs) %>%
-  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
-
+#edgeR_results_local_ERE = readRDS(file = glue::glue('{working_directory}/R_variables/edgeR_results_local_ERE'))
+#up_TEs = subset(edgeR_results_local_ERE, significant == T & logFC > 0)$locus
+#Filtered_Chimera = subset(Chimera, TEid %in% up_TEs) %>%
+#  mutate(RepeatID = forcats::fct_reorder(RepeatID, Contribution, .fun='mean'))
 
 contribution_plot = ggplot(data = Filtered_Chimera, aes(x = RepeatID, y = Contribution)) +
   geom_bar(stat = 'summary') +
@@ -150,34 +128,51 @@ contribution_plot + theme_bw() + theme(plot.title = element_text(face = 'bold', 
 ggsave("/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/Plots/LIONS_contribution_1.png", 
        width = 5, height = 6, units = "in")
 
-#################################################################
+################################################################################
 #  Contribution analysis: gene-centric
-#################################################################
+################################################################################
 
 mean_contribution = group_by(Filtered_Chimera, RepeatID) %>% 
   summarize(mean_contribution = mean(Contribution)) %>%
   subset(is.na(RepeatID) == F)
-mean_contribution = as.data.frame(mean_contribution) 
+mean_contribution = as.data.frame(mean_contribution)
 
 counter = 1
 gene_list = vector()
 contribution = vector()
 repeat_class = vector()
+repeat_name = vector()
+LO_occurence = vector()
+HI_occurence = vector()
+HI_specific = vector()
 
 for (i in 1:nrow(mean_contribution)){
   
   repeat_id = mean_contribution[i, 'RepeatID']
   
-  genes = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$Geneid), decreasing = TRUE)[1])
-  types = subset(Filtered_Chimera, RepeatID == repeat_id)[1, 'Gene_type']
+  genes = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$Geneid), decreasing = TRUE))
   
+  if (length(genes) > 1){
+    
+    longest = which.max(stringr::str_length(genes))
+    
+    genes = genes[longest]
+    
+  }
+  
+  specific = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$HI_specific), decreasing = TRUE))[1]
+
   genes = unlist(stringr::str_split(genes, pattern = ';'))
   
   for (a in 1:length(genes)){
     gene = stringr::str_trim(genes[a], side = 'both') 
     gene_list[counter] = gene
+    HI_specific[counter] = specific
     contribution[counter] = mean_contribution[i, 'mean_contribution']
     repeat_class[counter] = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$class), decreasing = TRUE)[1])
+    LO_occurence[counter] = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$LO_occurence), decreasing = TRUE)[1])
+    HI_occurence[counter] = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$HI_occurence), decreasing = TRUE)[1])
+    repeat_name[counter] = names(sort(table(subset(Filtered_Chimera, RepeatID == repeat_id)$repeatName), decreasing = TRUE)[1])
     
     counter = counter + 1
     
@@ -185,7 +180,10 @@ for (i in 1:nrow(mean_contribution)){
   
 }
 
-LIONS_genes = data.frame(ensembl_gene_id = gene_list, contribution = contribution, class = repeat_class) %>%
+LIONS_genes = data.frame(ensembl_gene_id = gene_list, contribution = contribution,
+                         repeat_name = repeat_name, repeat_class = repeat_class, 
+                         LO_occurence = LO_occurence, HI_occurence = HI_occurence,
+                         HI_specific = HI_specific) %>%
   mutate(ensembl_gene_id = forcats::fct_reorder(ensembl_gene_id, contribution, .fun='mean'))
 
 ## Annotate
@@ -195,13 +193,26 @@ ensembl = useEnsembl(biomart = "genes", dataset = 'hsapiens_gene_ensembl')
 conversion = getBM(attributes = c('ensembl_gene_id', 'gene_biotype', 'external_gene_name'), 
                    mart = ensembl, filter = 'ensembl_gene_id', values = LIONS_genes$ensembl_gene_id)
 
-input = merge(LIONS_genes, conversion, by = 'ensembl_gene_id')
+LIONS_genes_annotated = merge(LIONS_genes, conversion, by = 'ensembl_gene_id', all.x = T) 
 
-input = mutate(input, TRA_annotation = case_when(ensembl_gene_id %in% TRA_genes$ensembl_gene_id ~ 'TRA',
-                                      ensembl_gene_id %in% housekeeping_genes$ensembl_gene_id ~ 'Housekeeping',
-                                      T ~ 'None'))
+LIONS_genes_annotated = mutate(LIONS_genes_annotated, gene_biotype = case_when(gene_biotype == "transcribed_unitary_pseudogene" | gene_biotype == "transcribed_unprocessed_pseudogene" ~ 'Pseudogene',
+                                               gene_biotype == 'protein_coding' ~ 'Protein coding',
+                                               gene_biotype == 'lncRNA' ~ 'lncRNA',
+                                               T ~ gene_biotype))
+
+#LIONS_genes_annotated = mutate(LIONS_genes_annotated, TRA_annotation = case_when(ensembl_gene_id %in% TRA_genes$ensembl_gene_id ~ 'TRA',
+#                                      ensembl_gene_id %in% housekeeping_genes$ensembl_gene_id ~ 'Housekeeping',
+#                                      T ~ T))
+
+write.csv(LIONS_genes_annotated, '/Users/mpeacey/Desktop/thymus-epitope-mapping/ERE-analysis/analysis/gene_lists/LIONS_genes.csv')
 
 ## Visualize
+
+# All events
+input = LIONS_genes_annotated
+
+# mTEC-HI specific
+input = filter(LIONS_genes_annotated, HI_specific == T)
 
 r = "#FBB4AE"
 g = "#CCEBC5"
@@ -213,8 +224,8 @@ contribution_plot = ggplot(data = input, aes(x = ensembl_gene_id, y = contributi
   geom_text(aes(label = external_gene_name), angle = 90, nudge_y = 0.3, size = 4) +
   scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, .1))) +
   ylab('Repeat expression relative to gene') +
-  scale_fill_manual(labels = c('lncRNA', 'Protein-coding', 'Pseudogene'), values = c(r, g, p)) +
-  labs(fill= "")
+  labs(fill= "") +
+  scale_fill_manual(labels = c('lncRNA', 'Protein coding', 'Pseudogene'), values = c(r, g, p))
 
 contribution_plot + theme_bw() + theme(plot.title = element_text(face = 'bold', size = 20),
                                        plot.subtitle = element_text(size = 14),
